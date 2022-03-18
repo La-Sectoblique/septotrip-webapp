c28717e6a36965e6baa7e1dca4024787e78d0b47
Raffael Di Pietro <67447144+Augustin68@users.noreply.github.com>
2022-03-18 15:31:06 +0100

    Revert "Adding EsLint with Husky pre-push lint"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,23 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './modules/core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgxMapboxGLModule.withConfig({
-      accessToken: environment.mapBoxToken,
-    }),
-    FormsModule,
-    CoreModule,
+      accessToken: environment.mapBoxToken
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
