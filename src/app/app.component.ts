941af858f8b102c367c6719600c0dac1441e2c05
Raffael <maxou4170@gmail.com>
2022-03-18 14:29:28 +0100

    adding eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,6 @@ import { LngLatLike, MapMouseEvent } from 'mapbox-gl';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  ngOnInit(): void {
-      
-  }
 }
