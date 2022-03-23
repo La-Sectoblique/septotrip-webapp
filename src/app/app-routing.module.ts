f2c9836f486e050f4a00b550c97caba9a7f8b768
Raffael Di Pietro <raffael.di-pietro@etu.unistra.fr>
2022-03-23 22:25:22 +0100

    refactor(routing): setting up routing more properly for future

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,40 @@ import { HomeComponent } from './modules/core/pages/home/home.component';
 import { TripsComponent } from './modules/core/pages/trips/trips.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'trips', component: TripsComponent },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    // Put protected routes under this
+    path: '',
+    // canActivate: @todo: set routeGuards here
+    children: [
+      {
+        path: 'trips',
+        children: [
+          {
+            path: '',
+            component: TripsComponent,
+            pathMatch: 'full',
+          },
+          // { @example
+          //   path: ':tripsId',
+          //   component:,
+          // },
+        ],
+      },
+    ],
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
