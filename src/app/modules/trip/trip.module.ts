704fe6d813ed5b06b1b64460a1a8662f27d2c8fe
Raffael <maxou4170@gmail.com>
2022-03-24 17:39:26 +0100

    style(trip-preview): adding icons

--- src/app/modules/trip/trip.module.ts
+++ src/app/modules/trip/trip.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TripPreviewComponent } from './trip-preview/trip-preview.component';
+import { NbIconModule } from '@nebular/theme';
 
 @NgModule({
   imports: [
     CommonModule,
+    NbIconModule,
   ],
   declarations: [TripPreviewComponent],
   exports: [
