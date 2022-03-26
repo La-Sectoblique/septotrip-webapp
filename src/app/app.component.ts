e9e5c57ce60d0d8b9c640f903e496aacf49f8d5b
Raffael <maxou4170@gmail.com>
2022-03-26 14:59:38 +0100

    feat(token): creating localstorage token management service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { login, init } from '@la-sectoblique/septoblique-service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    console.log('app init');
+
+
+    const loginReturn = login({ email: 'jean', password: 'jean' });
+    console.log('loginreturn', loginReturn);
+  }
 
 }
