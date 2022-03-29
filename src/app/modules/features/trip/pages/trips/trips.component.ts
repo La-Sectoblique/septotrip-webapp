4b0616441a9cd3379a07b7d0f45ba0afe4e8e336
Raffael Di Pietro <raffael.di-pietro@etu.unistra.fr>
2022-03-29 22:21:12 +0200

    feat(trips): trips from server

--- src/app/modules/features/trip/pages/trips/trips.component.ts
+++ src/app/modules/features/trip/pages/trips/trips.component.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
-import { Trip } from '../../models/trip';
+import { Component, OnInit } from '@angular/core';
+import { TripOutput } from '@la-sectoblique/septoblique-service/dist/types/models/Trip';
+import { Observable } from 'rxjs';
+import { TripsService } from '../../services/trips.service';
 
 @Component({
   selector: 'spt-trips',
   templateUrl: './trips.component.html',
   styleUrls: ['./trips.component.scss'],
 })
-export class TripsComponent {
+export class TripsComponent implements OnInit {
 
-  trips: Trip[] = [
-    {
-      name: 'Voyage en France',
-      memberCount: 5,
-      stepCount: 3,
-      departDate: new Date('10-04-2022'),
-      arivalDate: new Date('12-04-2022'),
-      pictureSrc: 'https://www.meteociel.fr/cartes_obs/archives/23-03-2022/temp2_1h-18.png',
-    },
-  ];
+  trips: Observable<TripOutput[]>;
+
+  constructor(
+    private tripsService: TripsService,
+  ) {}
+
+  ngOnInit(): void {
+    this.trips = this.tripsService.getUserTrips();
+  }
 
 }
