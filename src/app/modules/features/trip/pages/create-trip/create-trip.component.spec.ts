18297da128599feba67099b698ef4eddcb020915
Raffael Di Pietro <raffael.di-pietro@etu.unistra.fr>
2022-03-29 22:36:18 +0200

    lint

--- src/app/modules/features/trip/pages/create-trip/create-trip.component.spec.ts
+++ src/app/modules/features/trip/pages/create-trip/create-trip.component.spec.ts
@@ -1,7 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateTripComponent } from './create-trip.component';
 
@@ -9,11 +7,11 @@ describe('CreateTripComponent', () => {
   let component: CreateTripComponent;
   let fixture: ComponentFixture<CreateTripComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateTripComponent ]
+      declarations: [ CreateTripComponent ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
