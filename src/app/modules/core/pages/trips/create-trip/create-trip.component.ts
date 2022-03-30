20d9450e2e87cc5574d7e854211d1b66a387524e
Thibaut Herr <thibaut.herr@etu.unistra.fr>
2022-03-30 12:31:49 +0200

    feat(trip): creating page for create trip

--- src/app/modules/core/pages/trips/create-trip/create-trip.component.ts
+++ src/app/modules/core/pages/trips/create-trip/create-trip.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-trip',
   templateUrl: './create-trip.component.html',
-  styleUrls: ['./create-trip.component.scss']
+  styleUrls: ['./create-trip.component.scss'],
 })
 export class CreateTripComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
