a8f5fcc42746be9a79f1d6a2d8780cffc926b41d
Raffael Di Pietro <raffael.di-pietro@etu.unistra.fr>
2022-04-01 02:04:20 +0200

    feat(map): tracing line between steps

--- src/app/modules/features/map/components/trips-map/trips-map.component.ts
+++ src/app/modules/features/map/components/trips-map/trips-map.component.ts
@@ -17,7 +17,7 @@ export class TripsMapComponent implements OnChanges {
 
 
   mapCenter: LngLatLike = [7.750149, 48.581551];
-  mapZoom: [number] = [13];
+  mapZoom: [number] = [7];
 
   selectedPoint: GeoJSON.Feature<GeoJSON.Point> | null = null;
   cursorStyle = '';
@@ -27,6 +27,42 @@ export class TripsMapComponent implements OnChanges {
   points: GeoJSON.FeatureCollection<GeoJSON.Point> = {
     type: 'FeatureCollection',
     features: [],
+  };
+
+  line: any = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'LineString',
+      coordinates:  [],
+    },
+  };
+
+  test = {
+    type: 'LineString',
+    coordinates: [
+      [-122.48369693756104, 37.83381888486939],
+      [-122.48348236083984, 37.83317489144141],
+      [-122.48339653015138, 37.83270036637107],
+      [-122.48356819152832, 37.832056363179625],
+      [-122.48404026031496, 37.83114119107971],
+      [-122.48404026031496, 37.83049717427869],
+      [-122.48348236083984, 37.829920943955045],
+      [-122.48356819152832, 37.82954808664175],
+      [-122.48507022857666, 37.82944639795659],
+      [-122.48610019683838, 37.82880236636284],
+      [-122.48695850372314, 37.82931081282506],
+      [-122.48700141906738, 37.83080223556934],
+      [-122.48751640319824, 37.83168351665737],
+      [-122.48803138732912, 37.832158048267786],
+      [-122.48888969421387, 37.83297152392784],
+      [-122.48987674713133, 37.83263257682617],
+      [-122.49043464660643, 37.832937629287755],
+      [-122.49125003814696, 37.832429207817725],
+      [-122.49163627624512, 37.832564787218985],
+      [-122.49223709106445, 37.83337825839438],
+      [-122.49378204345702, 37.83368330777276],
+    ],
   };
 
 
@@ -50,6 +86,15 @@ export class TripsMapComponent implements OnChanges {
         },
       })),
     };
+    this.line = {
+      ...this.line,
+      geometry: {
+        ...this.line.geometry,
+        coordinates: steps.currentValue.map((step: StepOutput) => step.localisation.coordinates),
+      },
+    };
+    console.log('new line', this.line);
+    console.log(this.test);
   }
 
 
--- src/app/modules/features/step/components/add-step/add-step.component.ts
+++ src/app/modules/features/step/components/add-step/add-step.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NbDialogRef } from '@nebular/theme';
 import { LngLat } from 'mapbox-gl';
 import { StepsService } from '../../services/steps.service';
