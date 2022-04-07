af0b1c60056365d19c2797b2915d396d3ec57f98
Thibaut Herr <thibaut.herr@etu.unistra.fr>
2022-04-07 02:56:56 +0200

    feat(login): start login page

---
+++ src/app/modules/core/services/account.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+import { environment } from "src/environments/environment.template";
+//import { User } from ./model
+
+@Injectable({ providedIn: 'root'})
+export class AccountService{
+    /*private userSubject: BehaviorSubject<User>;
+    public user: Observable<User>;
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ){
+        this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
+        this.user = this.userSubject.asObservable();
+    }*/
+}
