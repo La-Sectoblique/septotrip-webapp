24c4ebe404c56493d17a5b8fe002c65dd19d862a
Thibaut Herr <thibaut.herr@etu.unistra.fr>
2022-04-07 02:06:18 +0200

    merge with dev

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { TripModule } from '../trip/trip.module';
+//import { TripModule } from '../trip/trip.module';
 import { TripsComponent } from './pages/trips/trips.component';
 import { NbButtonModule, NbIconModule, NbInputModule } from '@nebular/theme';
-=======
-import { NbButtonModule, NbIconModule } from '@nebular/theme';
->>>>>>> 012ae19a71ee9e6c78724f76542fefc4a54d55b9
 
 @NgModule({
   imports: [
