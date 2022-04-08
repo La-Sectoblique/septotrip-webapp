424200d5f1c84056cf68746c4b2405bb2caafb33
Raffael <maxou4170@gmail.com>
2022-04-08 12:17:50 +0200

    feat(map-edit): adding edition button in step and points

--- src/app/modules/features/points/points.module.ts
+++ src/app/modules/features/points/points.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PointsListComponent } from './components/points-list/points-list.component';
 import { PointPreviewComponent } from './components/point-preview/point-preview.component';
-import { NbIconModule } from '@nebular/theme';
+import { NbButtonModule, NbIconModule } from '@nebular/theme';
 
 @NgModule({
   imports: [
     CommonModule,
+    NbIconModule,
+    NbButtonModule,
     NbIconModule,
   ],
   declarations: [
--- src/app/modules/features/step/components/steps-list/steps-list.component.ts
+++ src/app/modules/features/step/components/steps-list/steps-list.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { StepOutput } from '@la-sectoblique/septoblique-service/dist/types/models/Step';
 import { Store } from '@ngrx/store';
+import { first, Observable } from 'rxjs';
+import { MapEditMode } from 'src/app/modules/shared/models/map-edit-mode.enum';
+import { UpdateMapEditMode } from 'src/app/store/map-edit-store/state/map-edit.actions';
+import { selectMapEditMode } from 'src/app/store/map-edit-store/state/map-edit.selectors';
 import { DeleteTripStep } from 'src/app/store/trips-store/state/trips.actions';
 import { FlattenedStep } from '../../models/flattened-step';
 import { StepsService } from '../../services/steps.service';
@@ -10,15 +14,32 @@ import { StepsService } from '../../services/steps.service';
   templateUrl: './steps-list.component.html',
   styleUrls: ['./steps-list.component.scss'],
 })
-export class StepsListComponent {
+export class StepsListComponent implements OnInit {
 
   @Input() tripId: number;
-
   @Input() steps: FlattenedStep[] | null;
+
+  mapEditMode$: Observable<MapEditMode>;
+  mapEditMode = MapEditMode;
+
 
   constructor(
     private store: Store,
   ) {}
+
+  ngOnInit(): void {
+    this.mapEditMode$ = this.store.select(selectMapEditMode());
+  }
+
+  switchStepEditMode(): void {
+    this.mapEditMode$.pipe(first()).subscribe((editMode) => {
+      if (editMode !== MapEditMode.EDIT_STEPS) {
+        this.store.dispatch(UpdateMapEditMode({ mapMode: MapEditMode.EDIT_STEPS }));
+      } else {
+        this.store.dispatch(UpdateMapEditMode({ mapMode: MapEditMode.DEFAULT }));
+      }
+    });
+  }
 
 
   deleteStep(stepId: number): void {
