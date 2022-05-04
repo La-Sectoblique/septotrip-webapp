0faa0faad7ed82aa35fa8745aa2e594890ff72d0
Raffael Di Pietro <raffael.di-pietro@etu.unistra.fr>
2022-05-04 22:55:42 +0200

    feat(days): linking points list with days dropZone and dispatch updateTripPoint

--- src/app/modules/features/days/components/days-list/days-list.component.ts
+++ src/app/modules/features/days/components/days-list/days-list.component.ts
@@ -1,5 +1,9 @@
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit } from '@angular/core';
 import { DayOutput } from '@la-sectoblique/septoblique-service/dist/types/models/Day';
+import { PointOutput } from '@la-sectoblique/septoblique-service/dist/types/models/Point';
+import { Store } from '@ngrx/store';
+import { UpdateTripPoint } from 'src/app/store/trips-store/state/trips.actions';
 
 @Component({
   selector: 'spt-days-list',
@@ -9,5 +13,27 @@ import { DayOutput } from '@la-sectoblique/septoblique-service/dist/types/models
 export class DaysListComponent  {
 
   @Input() days: DayOutput[];
+  @Input() stepId: number;
+  @Input() tripId: number;
+
+  constructor(
+    private store: Store,
+  ) {}
+
+  itemDropped(event: CdkDragDrop<PointOutput>, dayId: number): void {
+    console.log('getted someting', event);
+
+    // @TODO: do something to put this fucking point in this fucking day haha
+    const eventData: PointOutput = event.item.data;
+    this.store.dispatch(UpdateTripPoint({
+      pointId: eventData.id,
+      tripId: this.tripId,
+      editedPoint: {
+        ...eventData,
+        stepId: this.stepId,
+        dayId,
+      },
+    }));
+  }
 
 }
--- src/app/modules/features/days/days.module.ts
+++ src/app/modules/features/days/days.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DaysListComponent } from './components/days-list/days-list.component';
 import { NbIconModule } from '@nebular/theme';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   imports: [
     CommonModule,
     NbIconModule,
+    DragDropModule,
   ],
   declarations: [
     DaysListComponent,
