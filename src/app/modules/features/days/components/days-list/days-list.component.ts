75e98dda517fa658aefc2e2b0f73a96ec3eebc8b
Raffael Di Pietro <raffael.di-pietro@etu.unistra.fr>
2022-05-12 22:10:00 +0200

    fix: removing reference to dayId temorary

--- src/app/modules/features/days/components/days-list/days-list.component.ts
+++ src/app/modules/features/days/components/days-list/days-list.component.ts
@@ -30,7 +30,7 @@ export class DaysListComponent  {
       editedPoint: {
         ...eventData,
         stepId: this.stepId,
-        dayId,
+        // dayId,
       },
     }));
   }
--- src/app/modules/shared/pipes/filter-points-by-dayId.pipe.ts
+++ src/app/modules/shared/pipes/filter-points-by-dayId.pipe.ts
@@ -5,7 +5,8 @@ import { PointOutput } from '@la-sectoblique/septoblique-service/dist/types/mode
 export class FilterPointsByDayIdPipe implements PipeTransform {
 
   transform(points: PointOutput[], dayId: number): PointOutput[] {
-    return points.filter((point) => point.dayId === dayId);
+    // return points.filter((point) => point.dayId === dayId);
+    return points;
   }
 
 }
