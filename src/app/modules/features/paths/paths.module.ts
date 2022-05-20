e355e6c04981fa45a28135841452349bc74465cb
Raffael <maxou4170@gmail.com>
2022-05-20 15:29:10 +0200

    refactor: lint

--- src/app/modules/features/paths/paths.module.ts
+++ src/app/modules/features/paths/paths.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PathsComponent } from './paths.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
   ],
-  declarations: [PathsComponent]
+  declarations: [],
 })
 export class PathsModule { }
