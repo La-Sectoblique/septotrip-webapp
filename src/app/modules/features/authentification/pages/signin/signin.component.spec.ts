0a055e1900b4258d8a197ee613568a3ad4d0e157
Raffael <maxou4170@gmail.com>
2022-05-20 15:31:53 +0200

    refactor: lint

--- src/app/modules/features/authentification/pages/signin/signin.component.spec.ts
+++ src/app/modules/features/authentification/pages/signin/signin.component.spec.ts
@@ -1,7 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SigninComponent } from './signin.component';
 
@@ -9,7 +7,7 @@ describe('SigninComponent', () => {
   let component: SigninComponent;
   let fixture: ComponentFixture<SigninComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SigninComponent ],
     })
--- src/app/modules/features/travelers/components/traveler-preview/traveler-preview.component.ts
+++ src/app/modules/features/travelers/components/traveler-preview/traveler-preview.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { UserOutput } from '@la-sectoblique/septoblique-service/dist/types/models/User';
-import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-traveler-preview',
