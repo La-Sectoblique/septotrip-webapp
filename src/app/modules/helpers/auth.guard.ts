d22f01ef119f41f4709ad1dd2091685de0802f2c
Raffael <maxou4170@gmail.com>
2022-06-09 16:58:52 +0200

    feat(auth): adding a info message when impossible to access page

--- src/app/modules/helpers/auth.guard.ts
+++ src/app/modules/helpers/auth.guard.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
+import { NbToastrService } from '@nebular/theme';
+import { TranslateService } from '@ngx-translate/core';
 import { AuthenticationService } from '../features/authentification/services/authentication.service';
 
 @Injectable({ providedIn: 'root' })
@@ -8,11 +10,17 @@ export class AuthGuard implements CanActivate {
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService,
+    private nbToastrService: NbToastrService,
+    private translateService: TranslateService,
   ) {}
 
   canActivate(): boolean {
     const { isUserLoggedIn } = this.authenticationService;
     if (!isUserLoggedIn) {
+      this.nbToastrService.info(
+        this.translateService.instant('LoginNeededMessage'),
+        this.translateService.instant('LoginNeeded'),
+      );
       this.router.navigate(['login']);
       return false;
     }
