64d428594a81a73c93a9e744a5621eaceede9bf2
Raffael <maxou4170@gmail.com>
2022-06-10 15:31:33 +0200

    feat(toastr): max toastr display count and prevent duplicated if user spam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -67,7 +67,7 @@ const appUserInitializer = (authenticationService: AuthenticationService) => () 
     NbLayoutModule,
     AuthentificationModule,
     HttpClientModule,
-    NbToastrModule.forRoot(),
+    NbToastrModule.forRoot({ limit: 3, duplicatesBehaviour: 'all', preventDuplicates: true }),
     PathsModule,
   ],
   providers: [
