b8c198e7d72b87d6c34480e3ae0590bf40404350
Thibaut Herr <thibaut.herr@etu.unistra.fr>
2022-06-10 15:09:45 +0200

    feat(toDoList): add Task

--- src/app/modules/features/toDoList/components/toDoList/toDoList.component.ts
+++ src/app/modules/features/toDoList/components/toDoList/toDoList.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { addTodoEntry, getTodoEntriesByTripId } from '@la-sectoblique/septoblique-service';
+import { TodoEntryOutput, TodoState } from '@la-sectoblique/septoblique-service/dist/types/models/Todo';
 
 @Component({
   selector: 'spt-todolist',
   templateUrl: './toDoList.component.html',
   styleUrls: ['./toDoList.component.scss'],
 })
-export class ToDoListComponent {
+export class ToDoListComponent implements OnInit {
 
+  @Input() tripId: number;
   //constructor() { }
+  taskName: string;
+  taskList: TodoEntryOutput[];
 
-  //ngOnInit() {
-  //}
+  ngOnInit() {
+    getTodoEntriesByTripId(this.tripId)
+      .then((res: TodoEntryOutput[]) => {
+        this.taskList = res;
+      });
+  }
+
+  addTask(taskName: string): void {
+    addTodoEntry({ state: TodoState.TODO, title: taskName, tripId: this.tripId })
+      .then((res: TodoEntryOutput) => {
+        this.taskList.push(res);
+        console.log('ici');
+      });
+  }
 
 }
